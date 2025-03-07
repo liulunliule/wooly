3cdd91f4d2bf2a12e7ee283de0048d8c7e6da72d
lieulunlieule <lieucao16122003@gmail.com>
2025-03-08 01:52:39 +0700

    update pagination

--- src/redux/features/cartSlice.js
+++ src/redux/features/cartSlice.js
@@ -1,9 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 import API_ROOT from "~/utils/constants";
-import { logout, logoutUser } from "./authSlice";
 
 const getAuthHeaders = (getState) => {
   const token = getState().auth.accessToken;
@@ -14,8 +12,6 @@ const getAuthHeaders = (getState) => {
 export const addToCart = createAsyncThunk(
   "cart/addToCart",
   async ({ productId, quantity, cartItems }, { rejectWithValue, getState }) => {
-    const dispatch = useDispatch();
-
     const state = getState();
     const token = state.auth.accessToken; 
 
@@ -35,9 +31,6 @@ export const addToCart = createAsyncThunk(
         }
       );
       console.log("addToCart",response);
-      if(response.data.status===500){
-        dispatch(logoutUser());
-      }
       
       toast.success(response.data);
       return response.data;
